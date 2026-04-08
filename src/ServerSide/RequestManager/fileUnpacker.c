df64401da327518c3ccb8d784f4c572b5bf42d84
Youdontsay111 <71226155+Youdontsay111@users.noreply.github.com>
2026-04-08 13:36:22 +0200

    fileunpacker
    frfr

--- src/ServerSide/RequestManager/fileUnpacker.c
+++ src/ServerSide/RequestManager/fileUnpacker.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fileUnpacker.h>
 
 
-typedef struct Data {
-    int id;
-    char text[1000];
-    double doors;
-    int x_size;
-    int y_size;
-    double branches;
-    double loops;
-    double straightness;
-}Data;
 
 Data unpackFile(int id)
 {
@@ -26,21 +17,22 @@ Data unpackFile(int id)
 
     if (file == NULL) {
         printf("Error opening file\n");
+        return; // better error handling maybe
     }
 
-    fread(data.text, sizeof(char), sizeof(data.text), file);
+    fscanf(file,
+    "{ \"Door\": %lf, \"x_size\": %d, \"y_size\": %d, \"branches\": %lf, \"loops\": %lf, \"straightness\": %lf }",
+    &data.doors,
+    &data.x_size,
+    &data.y_size,
+    &data.branches,
+    &data.loops,
+    &data.straightness
+);
+
     fclose(file);
 
 
-    sscanf(data.text,
-        "{ \"Door\": %lf, \"x_size\": %d, \"y_size\": %d, \"branches\": %lf, \"loops\": %lf, \"straightness\": %lf }",
-        &data.doors,
-        &data.x_size,
-        &data.y_size,
-        &data.branches,
-        &data.loops,
-        &data.straightness
-    );
 
 
     return data;

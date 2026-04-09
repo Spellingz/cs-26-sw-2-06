37c60efba091ccca935c18f634f4060f0d9fe5b6
XeniaLindegren <RF06GI@student.aau.dk>
2026-04-09 13:42:17 +0200

    Design
    Videreudvikling af design + virkning af kanpperne for parametrene

--- src/ClientSide/Design/FunctionDesign.js
+++ src/ClientSide/Design/FunctionDesign.js
@@ -1,2 +1,54 @@
+function DoorParameter() {
+    let html = `
+    <p>
+        <div id="Boks">
+            <button>Door Weight</button>
+        </div>
+    </p>
+    `;
+    document.getElementById("output").innerHTML = html;
+}
 
+function SizeParameter() {
+    let html = `
+    <p>
+        <div id="Boks">
+            <button>Size</button>
+        </div>
+    </p>
+    `;
+    document.getElementById("output").innerHTML = html;
+}
 
+function BranchesParameter() {
+    let html = `
+    <p>
+        <div id="Boks">
+            <button>Branches</button>
+        </div>
+    </p>
+    `;
+    document.getElementById("output").innerHTML = html;
+}
+
+function LoopsParameter() {
+    let html = `
+    <p>
+        <div id="Boks">
+            <button>Loops</button>
+        </div>
+    </p>
+    `;
+    document.getElementById("output").innerHTML = html;
+}
+
+function StraightnessParameter() {
+    let html = `
+    <p>
+        <div id="Boks">
+            <button>Straightness</button>
+        </div>
+    </p>
+    `;
+    document.getElementById("output").innerHTML = html;
+}

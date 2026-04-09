c072ed04e5dadaf0f405d278f177863245898711
Kian Stougaard Heftholm <kshspell@gmail.com>
2026-04-09 11:42:47 +0200

    Finished basic maze (probably)

--- src/ServerSide/MazeGeneration/generateMaze.c
+++ src/ServerSide/MazeGeneration/generateMaze.c
@@ -1,3 +1,4 @@
+#include <linux/limits.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -5,7 +6,7 @@
 // #include "mazeDataTypes.c"
 
 
-typedef struct ExportData {} ExportData;
+typedef struct ExportData {int x;} ExportData;
 typedef struct Data {} Data;
 
 typedef struct Wall Wall;
@@ -31,7 +32,7 @@ typedef struct Wall {
     char closedSides;
 } Wall;
 
-Wall defaultWall = {
+Wall DefaultWall = {
     .type = 1,
     .direction = 0,
     .isLoop = 0,
@@ -45,61 +46,59 @@ typedef struct Path {
 	Wall* walls;
 } Path;
 
-typedef struct mazeSize {
+typedef struct MazeSize {
     unsigned short x, y;
-} mazeSize;
+} MazeSize;
 
-typedef struct mazeWallSize {
+typedef struct MazeWallSize {
     long horizontal, vertical;
-} mazeWallSize;
+} MazeWallSize;
 // ...
 
-typedef struct mazeStruct {
+typedef struct MazeStruct {
+    MazeWallSize wallSize;
     Wall *horizontalArr;
     Wall *verticalArr;
-} mazeStruct;
+} MazeStruct;
 
-mazeStruct *fillWalls(mazeWallSize wallSize)
+MazeStruct *fillWalls(MazeSize size)
 {
-    mazeStruct *maze = malloc(sizeof(mazeStruct));
-    *maze = (mazeStruct) {
-        malloc(sizeof(Wall)*wallSize.horizontal),
-        malloc(sizeof(Wall)*wallSize.vertical),
-    };
+    MazeWallSize wallSize = {(size.y-1)*size.x, (size.x-1)*size.y}; 
+    MazeStruct *maze = malloc(sizeof(MazeStruct));
+    Wall *block = malloc(sizeof(Wall)*wallSize.horizontal + sizeof(Wall)*wallSize.vertical + sizeof(Wall));
+    maze->wallSize = wallSize;
+    maze->horizontalArr = block;
+    maze->verticalArr = block + maze->wallSize.horizontal;
 
-    for (int i = 0; i < wallSize.horizontal; i++)
+    for (int i = 0; i < maze->wallSize.horizontal; i++)
     {
-        maze->horizontalArr[i] = defaultWall;
+        maze->horizontalArr[i] = DefaultWall;
     }
-    for (int j = 0; j < wallSize.vertical ; j++)
+    for (int j = 0; j < maze->wallSize.vertical ; j++)
     {
-        maze->verticalArr[j] = defaultWall;
+        maze->verticalArr[j] = DefaultWall;
     }
     return maze;
 }
 
-
-
-
-int getHorizontalArrIndex(Point pos, mazeSize size)
+int getHorizontalArrIndex(Point pos, MazeSize size)
 {
     if (pos.x == size.x || pos.y == size.y)
         return -1;
 
-    return pos.x + pos.y*(size.x-1)+1;
+    return pos.x + pos.y*(size.x-1);
 }
 
-int getVerticalArrIndex(Point pos, mazeSize size)
+int getVerticalArrIndex(Point pos, MazeSize size)
 {
 
     if (pos.x == size.x || pos.y == size.y)
         return -1;
 
-    return pos.x + pos.y*(size.x-1)+1;
+    return pos.y + pos.x*(size.y-1);
 }
 
-
-Wall **getNeighbourWalls(mazeStruct *maze, mazeSize size, Point pos)
+Wall **getNeighbourWalls(MazeStruct *maze, MazeSize size, Point pos)
 {
     Wall **neighbourWalls = malloc(sizeof(Wall)*4);
 
@@ -127,9 +126,8 @@ Wall **getNeighbourWalls(mazeStruct *maze, mazeSize size, Point pos)
 }
 
 
-void addNeighbourFrontiers(Wall **frontier, int *frontierSize, mazeStruct *maze, mazeSize size, Point pos)
+void addNeighbourFrontiers(Wall **frontier, int *frontierSize, MazeStruct *maze, MazeSize size, Point pos)
 {
-        
     Wall **neighbourWalls = getNeighbourWalls(maze, size, pos);
 
     for (int i = 0; i < 4; i++)
@@ -139,6 +137,7 @@ void addNeighbourFrontiers(Wall **frontier, int *frontierSize, mazeStruct *maze,
         neighbourWalls[i]->direction = (i+1)%2;
         frontier[(*frontierSize)++] = neighbourWalls[i];
     }
+    free(neighbourWalls);
 }
 
 
@@ -154,17 +153,43 @@ Wall *popRandomFrontier(Wall **frontier, int *frontierSize)
     return poppedFrontier;
 }
 
+typedef struct ArrIndexResult {
+    bool isHorizontal;
+    int index;
+} ArrIndexResult;
+
+ArrIndexResult getArrayIndex(MazeStruct *maze, Wall *frontierWall)
+{
+    if (frontierWall >= maze->verticalArr && frontierWall < maze->verticalArr + maze->wallSize.vertical) 
+        return (ArrIndexResult){0, (int)(frontierWall - maze->verticalArr)};
+    else if (frontierWall >= maze->horizontalArr && frontierWall < maze->horizontalArr + maze->wallSize.horizontal)
+        return (ArrIndexResult){1, frontierWall - maze->horizontalArr};
+    else {
+        printf("No Index Found!");
+        return (ArrIndexResult){0, -1}; //FallBack
+    }
+}
+
+Point indexToPos(bool isHorizontal, int index, MazeSize mazeSize)
+{
+    if (isHorizontal)
+        //Horizontal
+        return (Point){index % (mazeSize.x-1), index / (mazeSize.x-1)};
+    else
+        return (Point){index / (mazeSize.y-1), index % (mazeSize.y-1)};
+}
+
 ExportData generateMaze(Data data)
 {
-    mazeSize size = {data.size.x, data.size.y};
-    mazeWallSize wallSize = {(size.y-1)*size.x, (size.x-1)*size.y};
-    mazeStruct *maze = fillWalls(wallSize);
+    // MazeSize size = {data.size.x, data.size.y};
+    MazeSize size = {1,1};
+    MazeStruct *maze = fillWalls(size);
     
 
     int frontierSize = 0;
     Wall** frontiers = malloc(
-        sizeof(Wall)*wallSize.horizontal+
-        sizeof(Wall)*wallSize.vertical
+        sizeof(Wall)*maze->wallSize.horizontal+
+        sizeof(Wall)*maze->wallSize.vertical
         );
     
     Point startPos = {1,1};
@@ -174,6 +199,20 @@ ExportData generateMaze(Data data)
     {
         Wall *rndFrontier = popRandomFrontier(frontiers, &frontierSize);
         if (rndFrontier->closedSides >= 2) continue;
+        
+        ArrIndexResult arrIndex = getArrayIndex(&*maze, rndFrontier);
+        if (arrIndex.index == -1) continue;
+
+        // frontierPos = Index + direction
+        Point frontierPos = indexToPos(arrIndex.isHorizontal, arrIndex.index + rndFrontier->direction, size);
+
+        addNeighbourFrontiers(frontiers, &frontierSize, &*maze, size, frontierPos);
+
         rndFrontier->type = AIR;
     }
+    free(frontiers);
+    free(maze->horizontalArr);
+    free(maze);
+
+    return (ExportData){NULL};
 }

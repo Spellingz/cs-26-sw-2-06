f26caa14e458211076b386413d76d206341e8e1f
Youdontsay111 <71226155+Youdontsay111@users.noreply.github.com>
2026-04-16 12:13:19 +0200

    Sent input info til serveren

--- src/ClientSide/Design/FunctionDesign.js
+++ src/ClientSide/Design/FunctionDesign.js
@@ -1,3 +1,10 @@
+let maze = {
+    horizontalWalls: [],
+    verticalWalls: [],
+    height: 0,
+    width: 0
+};
+
 function DoorParameter() {
     let html = `
     <div id="Boks">
@@ -236,3 +243,41 @@ function SaveInformationInputStraightness () {
 }
 
 //functions for the heat-map buttons
+
+
+//Gets the variables from the different input parameters
+
+function sendmazeinfo(){
+    let door = document.getElementById('rangeDoor').value;
+    let height = document.getElementById('HeightInput').value;
+    let width = document.getElementById('WidthInput').value;
+    let branches = document.getElementById('rangeBranches').value;
+    let loops = document.getElementById('rangeLoops').value;
+    let straightness = document.getElementById('rangeStraightness').value;
+
+
+    let mazeVariables = {
+        'SessionId': 1,
+        'Door': door,
+        'Size_Width': width,
+        'Size_height': height,
+        'Branches': branches,
+        'Loops': loops,
+        'Straightness': straightness
+    }
+
+    fetch ('WWW.com', {method: 'POST', body: JSON.stringify(mazeVariables)})
+    .then(response => response.json())
+    .then(response => {
+        maze.horizontalWalls = response.serverHorizontalWalls;
+        maze.verticalWalls = response.serverVerticalWalls;
+        maze.height = height;
+        maze.width = width;
+    })
+
+
+}
+
+
+
+

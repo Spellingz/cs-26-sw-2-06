63bfd45f8849f3e94a1310eafdd58910ae83828f
XeniaLindegren <RF06GI@student.aau.dk>
2026-04-23 20:01:23 +0200

    more button function
    started on the more button

--- src/ClientSide/Design/MoreAndAbout.js
+++ src/ClientSide/Design/MoreAndAbout.js
@@ -20,3 +20,28 @@ function closePage() {
 }
 
 
+function more() {
+    let html = `
+        <nav>
+            <div class="menu">
+                <ul class="nav-links">
+                    <button onclick="closePage()" style="background-color: #f0efeb; border: 6px solid #d8d7d3; border-radius: 12px; height: 10%; width: 3%; font-weight: bold;" >x</button>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Servises</a></li>
+                    <li><a href="#">Settings</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+
+
+    `;
+    const output = document.getElementById("navigationOutput");
+    output.innerHTML = '';
+    output.insertAdjacentHTML('beforeend', html);
+}
+function closePage() {
+    const output = document.getElementById("navigationOutput");
+    output.innerHTML = '';
+}
+

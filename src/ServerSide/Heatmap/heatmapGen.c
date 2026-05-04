da2468623256ad31b1eb93a8d3609e953ee93e5d
Sullz <frederik.schultzzz@gmail.com>
2026-05-04 12:23:25 +0200

    Working distance from middle calc

--- src/ServerSide/Heatmap/heatmapGen.c
+++ src/ServerSide/Heatmap/heatmapGen.c
@@ -14,18 +14,17 @@ void checkHeat(MazeStruct maze) {
 		for (int j = 0; j < (int)maze.wallCount.horizontal; j++) {
 			mazeSpots[k].posX = j;
 			mazeSpots[k].posY = i;
-			mazeSpots[k].heat = ceil(((abs(j-floor(maze.wallCount.horizontal/2))+abs(i-floor(maze.wallCount.vertical/2)))/(floor(maze.wallCount.horizontal/2)+floor(maze.wallCount.vertical)))*6);
+			mazeSpots[k].heat = ceil(((abs(j-(maze.wallCount.horizontal/2))+abs(i-(maze.wallCount.vertical/2)))/((maze.wallCount.horizontal/2)+(maze.wallCount.vertical)))*6);
 			if (i == 19 && j == 19) {
 				printf("\n\nMax width divided by two: %ld", maze.wallCount.horizontal/2);
-				printf("\nRounded down: %f", floor(maze.wallCount.horizontal/2));
-				printf("\nSubtract horizontal position: %f", j-floor(maze.wallCount.horizontal/2));
-				printf("\nAbsolute value: %d", abs(j-floor(maze.wallCount.horizontal/2)));
+				printf("\nSubtract horizontal position: %f", j-(maze.wallCount.horizontal/2));
+				printf("\nAbsolute value: %d", abs(j-(maze.wallCount.horizontal/2)));
+
 				printf("\n\nMax height divided by two: %ld", maze.wallCount.vertical/2);
-				printf("\nRounded down: %f", floor(maze.wallCount.vertical/2));
-				printf("\nSubtract vertical position: %f", i-floor(maze.wallCount.vertical/2));
-				printf("\nAbsolute value: %d", abs(i-floor(maze.wallCount.vertical/2)));
+				printf("\nSubtract vertical position: %f", i-(maze.wallCount.vertical/2));
+				printf("\nAbsolute value: %d", abs(i-(maze.wallCount.vertical/2)));
 				
-				printf("\n\nRounded up value of %f", ((abs(j-floor(maze.wallCount.horizontal/2))+abs(i-floor(maze.wallCount.vertical/2)))/(floor(maze.wallCount.horizontal/2)+floor(maze.wallCount.vertical)))*6);
+				printf("\n\nRounded up value of %f", ceil(((abs(j-(maze.wallCount.horizontal/2))+abs(i-(maze.wallCount.vertical/2)))/((maze.wallCount.horizontal/2)+(maze.wallCount.vertical)))*6));
 				printf("\nResulting in heat index %d", mazeSpots[k].heat);
 			}
 			k++;

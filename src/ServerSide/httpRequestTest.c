47ccf663dcae329eebe2acccc8ffa9291ed77dec
AlexDaunorasNielsen <adni25@student.aau.dk>
2026-05-06 14:31:41 +0200

    Hot fix

--- src/ServerSide/httpRequestTest.c
+++ src/ServerSide/httpRequestTest.c
@@ -171,29 +171,23 @@ static enum MHD_Result process_post (void *coninfo_cls,
     connection_info_struct *con_info = coninfo_cls;
 
     // RETURN IF KEY DOESN'T MATCH PREFERENCE
-<<<<<<< Maze-alteration
-    char *keyArray[] = {"type", "id", "door", "x_size", "y_size", "branches", "loops", "straightness", "isHorizontal", "wallIndex", "alterationType", "perfectMaze", NULL};
-=======
-    char *keyArray[9] = {
-        "type", 
-        "id", 
-        "door", 
-        "x_size", 
-        "y_size", 
-        "branches", 
-        "loops", 
-        "straightness", 
-        NULL
-        };
->>>>>>> main
+    char *keyArray[] = {"type",
+        "id",
+        "door",
+        "x_size",
+        "y_size",
+        "branches",
+        "loops",
+        "straightness",
+        "isHorizontal",
+        "wallIndex",
+        "alterationType",
+        "perfectMaze",
+        NULL};
+
     int keyIndex = findKey(key, keyArray);
     if (keyIndex == -1)
         return MHD_YES;
-
-<<<<<<< Maze-alteration
-    if (VERBOSITY == ALL) printf("kvp: %s, %s\n", key, data);
-=======
->>>>>>> main
 
     // CONTINUOUSLY ADD CORRECT KEY DATAVALUES INTO jsonData IF CORRECT SIZE
     if ((dataSize > 0) && (dataSize <= 20))

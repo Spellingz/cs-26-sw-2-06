61313a6bd50bcb5ca34525ce6ce17d0ceb43bc55
Sullz <frederik.schultzzz@gmail.com>
2026-05-25 13:18:49 +0200

    Explode your dick and balls

--- src/ServerSide/MazeAlteration/changeMaze.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHANGEMAZE_H
-#define CHANGEMAZE_H
-#include <stdio.h>
-#include <stdlib.h>
-#include "../DataTypes/requestDataTypes.h"
-
-AlterationExportData AlterMaze(AlterationData data);
-
-#endif
--- src/ServerSide/MazeGeneration/generateMaze.h
+++
@@ -1,7 +0,0 @@
-#ifndef GENERATEMAZE_H
-#define GENERATEMAZE_H
-#include "../DataTypes/requestDataTypes.h"
-
-ExportData GenerateMaze(GenerationData data);
-
-#endif
